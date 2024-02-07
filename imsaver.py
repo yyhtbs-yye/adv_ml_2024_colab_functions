0ed7a3e44c120ef81c716c963f3278d8896e55c1
Yuhang Ye <74354145+yyhtbs-yye@users.noreply.github.com>
2024-02-07 16:33:02 +0000

    Update imsaver.py

--- imsaver.py
+++ imsaver.py
@@ -2,16 +2,16 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 
-def save_grid_images(inps, name_dict, labels, predicted, nrow=8, show_only_incorrect=False):
+def save_grid_images(images, labels, predicted, name_dict, nrow=8, show_only_incorrect=False):
     if show_only_incorrect:
         # Filter to include only incorrect predictions
         incorrect_indices = [i for i, (l, p) in enumerate(zip(labels, predicted)) if l != p]
-        inps = inps[incorrect_indices]
+        images = images[incorrect_indices]
         labels = [labels[i] for i in incorrect_indices]
         predicted = [predicted[i] for i in incorrect_indices]
         
     titles = [f'T:{name_dict[l]}/P:{name_dict[p]}' for l, p in zip(labels, predicted)]
-    n_images = inps.shape[0]
+    n_images = images.shape[0]
     ncol = int(np.ceil(n_images / nrow))
     
     fig = make_subplots(rows=ncol, cols=nrow, subplot_titles=titles)
@@ -20,7 +20,7 @@ def save_grid_images(inps, name_dict, labels, predicted, nrow=8, show_only_incor
     for r in range(1, ncol + 1):
         for c in range(1, nrow + 1):
             if img_idx < n_images:
-                fig.add_trace(go.Image(z=inps[img_idx]), row=r, col=c)
+                fig.add_trace(go.Image(z=images[img_idx]), row=r, col=c)
             img_idx += 1
     
     # Update layout to hide axis ticks and labels
