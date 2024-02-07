8841c17144a62926f4f80d644a7257180d9701b8
Yuhang Ye <74354145+yyhtbs-yye@users.noreply.github.com>
2024-02-07 16:33:55 +0000

    Update evaluation_utils.py

--- evaluation_utils.py
+++ evaluation_utils.py
@@ -1,6 +1,6 @@
 import torch 
 
-def get_labels_and_predictions(model, dataloader, to_cpu=True):
+def get_all(model, dataloader, to_cpu=True):
     model.eval()  # Set the model to evaluation mode
     with torch.no_grad():
         correct = 0
@@ -13,11 +13,11 @@ def get_labels_and_predictions(model, dataloader, to_cpu=True):
 
             # Convert the batch of X and predictions to CPU for visualization
             if to_cpu:
-                # X = X.cpu()
+                X = X.cpu()
                 y = y.cpu()
                 y_hat = y_hat.cpu()
 
-    return y, y_hat
+    return X, y, y_hat
 
 def unnormalize(images, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     # The mean and std have to be reshaped to [C, 1, 1] to match the tensor dimensions.
