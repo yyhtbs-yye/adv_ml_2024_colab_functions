0b51a4944f73e395ea9362b56f2ce2bd600a7584
Yuhang Ye <74354145+yyhtbs-yye@users.noreply.github.com>
2024-02-11 00:56:24 +0000

    Update evaluation_utils.py

--- evaluation_utils.py
+++ evaluation_utils.py
@@ -1,7 +1,5 @@
 import torch 
 
-import torch
-
 def get_all(model, dataloader, to_cpu=True):
     model.eval()  # Set the model to evaluation mode
     Xs, ys, y_hats = [], [], []  # Initialize empty lists to accumulate batches
@@ -12,7 +10,7 @@ def get_all(model, dataloader, to_cpu=True):
                 X, y = X.cuda(), y.cuda()
             
             y_hat = model(X)
-            _, predicted = torch.max(y_hat.data, 1)
+            _, predicted = torch.max(y_hat.data, 1) # [0: B, 1: C]
             
             if to_cpu:
                 X = X.cpu()
@@ -25,11 +23,12 @@ def get_all(model, dataloader, to_cpu=True):
             y_hats.append(predicted)
 
     # Concatenate all accumulated batches
-    Xs = torch.cat(Xs, dim=0)
+    Xs = torch.cat(Xs, dim=0) # [Xs, Xs, Xs] Xs.shape=[B, C], [[B, C], ...[B, C]] -> [K*B, C]
     ys = torch.cat(ys, dim=0)
     y_hats = torch.cat(y_hats, dim=0)
     
     return Xs, ys, y_hats
+
 def unnormalize(images, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     # The mean and std have to be reshaped to [C, 1, 1] to match the tensor dimensions.
     mean = torch.tensor(mean).view(1, -1, 1, 1)
@@ -41,3 +40,20 @@ def unnormalize(images, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     
     images.mul_(std).add_(mean)  # This modifies the tensor in-place
     return images
+
+def get_imbalanced_dataset_weight(dataset):
+    class_counts = {}
+    for it in dataset:
+        _, label = it  # Assuming your dataset has a 'targets' attribute
+        if label in class_counts:
+            class_counts[label] += 1
+        else:
+            class_counts[label] = 1
+
+    # Calculate weight for each sample in the training set
+    weights = []
+    for it in dataset:
+        _, label = it
+        weights.append(1 / class_counts[label])
+    
+    return weights
